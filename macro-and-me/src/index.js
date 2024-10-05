4db74b6b0f366bc912f2630e2b167d33d1ae0bfa
Lin Latt <lattln.tech@gmail.com>
2024-10-05 00:45:07 -0400

    initial commit
    + groundwork
    + react
    + express
    + tailwind css
    +file structure
    + basic routing
    + mock prototype page

---
+++ macro-and-me/src/index.js
@@ -0,0 +1,10 @@
+
+import './global.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/App';
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
