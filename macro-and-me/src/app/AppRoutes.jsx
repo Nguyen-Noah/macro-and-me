f59c74bd8064a5bdb460846e76c538470a38167e
myeken <114724583+myeken@users.noreply.github.com>
2024-10-07 18:35:12 -0400

    Implemented USDA API w/ Search page
    Integrated USDA API with search bar to search up food items.

--- macro-and-me/src/app/AppRoutes.jsx
+++ macro-and-me/src/app/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'; 
 import Home from '../pages/Home.jsx';
+import FoodSearch from '../pages/FoodSearch.jsx';
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/search" element={<FoodSearch />} />
         </Routes>
     );
 }
