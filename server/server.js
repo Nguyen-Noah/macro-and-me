d4802c1453e89b4f00677458d1f6df52629ec3ef
Noah Nguyen <nohanguyenn@gmail.com>
2024-12-07 15:45:31 -0500

    fix server.js file serving

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 const { json } = express;
 import path from 'path';
+import { fileURLToPath } from 'url';
 import userRoutes from './routes/userRoutes.js';
 import imageUpload from './routes/imageUpload.js'
 import connectDB from './config/databaseFactory.js';
@@ -13,9 +14,13 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Middleware
 app.use(cors());
 app.use(json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
