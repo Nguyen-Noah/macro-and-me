0da11354a6972dc61e3779c72dc8fa37a5c81970
Noah Nguyen <nohanguyenn@gmail.com>
2024-12-07 19:12:25 -0500

    reordering file serving in the server

--- server/server.js
+++ server/server.js
@@ -21,20 +21,19 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-});
-
 // Routes
 app.use('/api', userRoutes);
 app.use('/api', mealRoutes);
 app.use('/api', imageUpload);
 app.use('/api', logRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-//node server.js
