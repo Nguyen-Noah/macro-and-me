f009e52616773e33b3ace6c2f2a3d6e4a98dbf3d
Noah Nguyen <nohanguyenn@gmail.com>
2024-12-08 23:39:27 -0500

    set up configuration for ci pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,15 @@
-export const modulePathIgnorePatterns = ['<rootDir>/macro-and-me/package.json'];
+module.exports = {
+    testEnvironment: 'node',
+  
+    // Match only test files in the __test__ directory or other specified locations
+    transform: {
+      '^.+/__test__/.*\\.js$': 'babel-jest',  // Apply babel-jest to JS files in the __test__ folder
+    },
+  
+    // Ignore transform for non-test files
+    transformIgnorePatterns: [
+      '/node_modules/',
+    ],
+  
+  };
+  
